07beb9105a31a09060c00a729a0fa53aaf3a2765
Ashish Kumar <akumar@ampliforce.com>
2022-11-05 19:11:20 +0530

    added automatic deploy to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { AuthService } from 'src/app/_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angular-assignment';
-  constructor(
-    private auth: AuthService
-  ) { }
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {
-    // this.auth.print();
-    // console.log(this.auth.autoSignIn())
-  }
+    // Import the functions you need from the SDKs you need
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
 
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDUMlJxk1lrEEdnQTIwroGSKtrMP_pGZGM',
+      authDomain: 'angular-assignment-2-f3ef2.firebaseapp.com',
+      databaseURL:
+        'https://angular-assignment-2-f3ef2-default-rtdb.firebaseio.com',
+      projectId: 'angular-assignment-2-f3ef2',
+      storageBucket: 'angular-assignment-2-f3ef2.appspot.com',
+      messagingSenderId: '602037431254',
+      appId: '1:602037431254:web:b8d9e8d4f6348e15469277',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+  }
 }
