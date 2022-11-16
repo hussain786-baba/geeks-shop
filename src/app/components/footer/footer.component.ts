5e8dd00338c892c54ba38458993643f0c9ba4622
Nabullah Ansari <nabullarock@gmail.com>
2022-11-16 22:51:44 +0530

    design changes on footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from 'src/app/_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { SubjectBehaviourService } from 'src/app/_services/subject-behaviour.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor(
-    private _auth:AuthService,
-  ) { }
+  admin_dashboard: boolean = false
+  constructor(private _auth: AuthService, private subjectBehaviourService: SubjectBehaviourService
+  ) {
+    this.subjectBehaviourService.admin_dashboard_navColor$.subscribe((res) => {
+      if (res == '/admin-dashboard/dashboard') {
+        console.log(res);
+        this.admin_dashboard = true;
+      } else {
+        this.admin_dashboard = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this._auth.autoSignIn();
+    this.admin_dashboard = false;
   }
 
 }
