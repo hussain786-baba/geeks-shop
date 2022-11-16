a78c250bca07eec4d3d562d015d3168d6570935c
Ashish Kumar <akumar@ampliforce.com>
2022-11-16 16:19:09 +0530

    design changes

--- src/app/_services/errorhandling.service.ts
+++ src/app/_services/errorhandling.service.ts
@@ -10,10 +10,9 @@ export class ErrorhandlingService {
     UNKNOWN: 'Unknown Error Occured, Please check your Internet',
     EMAIL_EXISTS: 'The email address is already in use by another account.',
     OPERATION_NOT_ALLOWED: 'Password sign-in is disabled for this project.',
-    TOO_MANY_ATTEMPTS_TRY_LATER: 'We have blocked all requests from this device due to unusual activity. Try again later.',
     EMAIL_NOT_FOUND: 'Wrong Email or Password :  Please check your Email and Password ',
     INVALID_PASSWORD: 'Wrong Email or Password :  Please check your Email and Password ',
     USER_DISABLED: 'The user account has been disabled by an administrator. Please Contact Customer Care',
-    
+    TOO_MANY_ATTEMPTS_TRY_LATER :"Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later"
   }
 }
--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { SubjectBehaviourService } from 'src/app/_services/subject-behaviour.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
-export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AdminDashboardComponent implements OnInit, OnDestroy {
+  constructor(private title: Title, public router: Router, private subjectBehaviuorSer:SubjectBehaviourService) {
+    this.title.setTitle('Admin Dashboard - Geeks Shop');
+    let adminDashboard = this.router.url;
+    this.subjectBehaviuorSer.admin_dashboard_navColor$.next(adminDashboard)
   }
 
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.title.setTitle('Geeks Shop | All is Well that ends well');
+    this.subjectBehaviuorSer.admin_dashboard_navColor$.next('')
+
+  }
 }
