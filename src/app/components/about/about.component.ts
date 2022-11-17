308831d6fed95b332f7ecf6462a0dde9d02ac54d
Ashish Kumar <akumar@ampliforce.com>
2022-11-17 16:13:14 +0530

    Redesigned About page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faChessQueen, faHand } from '@fortawesome/free-regular-svg-icons';
+import { faFile, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  faChess = faChessQueen;
+  faHandShake = faHand;
+  faFile = faFile;
+  faGlobe = faGlobe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/about/about.module.ts
+++ src/app/components/about/about.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { RouterModule } from '@angular/router';
+import { NzZorroModule } from 'src/app/_models_and_interface/nz-zorro.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const routes = [
   {
@@ -15,7 +17,7 @@ const routes = [
     AboutComponent
   ],
   imports: [
-    CommonModule, RouterModule.forChild(routes),
+    CommonModule, RouterModule.forChild(routes), NzZorroModule,FontAwesomeModule
   ]
 })
 export class AboutModule { }
